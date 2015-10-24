1f36697b37ff29394f1d2c81db49fb1529349769
ktmellow <letsspamkt@gmail.com>
2015-10-24 04:25:06 -0700

    first commit, with sample info

--- app.js
+++ app.js
@@ -40,3 +40,24 @@ app.get('/puppies', function(req, res) {
 // Start the app on port 5000
 app.listen(5000, '0.0.0.0');
 console.log("App is now listening at http://localhost:5000");
+
+/*
+profile = {
+    fullName: “Clarence Leung”,
+    profilePhoto: "https://avatars2.githubusercontent.com/u/686352?v=3&s=460",
+    about: 
+    {
+        region: “San Mateo Convention Center”,
+        age: “30”,
+        bio: “As a single father, raising Sophie by myself, I needed all the help I could get. \
+         My jobs don't pay well, so we live paycheck to paycheck. \
+         I want to make sure that my daughter can afford the life I never had. \
+         Every morning I drop a quarter into a piggy bank labelled Sophie's Educational Fund. \
+         I want her to grow up to be a confident leader and passionate learner. So every moment I spend with her, I make it count. \
+         I have always relied on my close friends and family for advice but sometimes even they didn't have all the answers. \
+         After all, it takes a village to raise a child. And that's when I found Smilestones.”
+    }
+    recipesInvolved: [123] // parent documents each recipe they are trying: their own and others
+}
+
+*/
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,6 +1,40 @@
-module.exports = {
+/*module.exports = {
     "123": {
         fullName: "Katherine Low",
         tags: ['problem solving', 'math', 'counting']
+    }
+};
+*/
+
+module.exports = {   
+	"123": {
+        author: "MamaColette", 
+        title: "Countdown To Counting",
+        submitDate: "2015-10-23T07:00:00.000Z", 
+        startAge: "0-1 years old", 
+        endAge: "4 years old",
+        recipe: [
+                "Count everything. Count how many blocks your toddler can stack. Or the number of steps in your house. \
+                  Or his fingers and toes. Make a habit of counting out loud, and soon he or she will join in.",
+                "Have your child setup the kitchen table. Matching dinner objects according to number. \
+                  Encourage your little host/hostess to count while handing out the supplies \
+                  and the child will acquire math skills without even realizing it.",
+                "Sing a nursery rhyme with counting. Learn as many tunes as you can, or make up your own verses ('Five little \
+                  monkeys jumping on the bed. One fell off and bumped his head. Called the doctor and the doctor said\
+                  No more monkeys jumping on the bed!')"    
+                ],
+        resources: [
+                "https://www.youtube.com/watch?v=Vr-L6q4vbWU", 
+                "http://www.target.com/p/plantoys-numbers-1-10/-/A-16540185#prodSlot=medium_1_8&term=counting+toys",
+                "http://motherhood.modernmom.com/fun-ways-teach-math-three-year-old-15797.html"
+                ],
+        milestones: ["counting"],
+        upvotes: 27, 
+        tags: [
+               "counting", 
+               "math", 
+               "music", 
+               "singing"
+            ]
     }
 };
