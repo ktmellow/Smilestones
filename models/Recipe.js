9cf3024e52955bb7fa433e8c968421458e0f0dd4
colette pitamba <colette.pitamba@gmail.com>
2015-10-24 18:52:06 -0700

    initial commit

--- models/Recipe.js
+++ models/Recipe.js
@@ -7,13 +7,13 @@
 */
 
 module.exports = {   
-	"123": {
-        author: "MamaColette", 
-        title: "Countdown To Counting",
-        submitDate: "2015-10-23T07:00:00.000Z", 
-        startAge: "0-1 years old", 
-        endAge: "4 years old",
-        recipe: [
+  "123": {
+        "author": "MamaColette", 
+        "title": "Countdown To Counting",
+        "submitDate": "2015-10-23T07:00:00.000Z", 
+        "startAge": "0-1 years old", 
+        "endAge": "4 years old",
+        "recipe": [
                 "Count everything. Count how many blocks your toddler can stack. Or the number of steps in your house. \
                   Or his fingers and toes. Make a habit of counting out loud, and soon he or she will join in.",
                 "Have your child setup the kitchen table. Matching dinner objects according to number. \
@@ -23,21 +23,21 @@ module.exports = {
                   monkeys jumping on the bed. One fell off and bumped his head. Called the doctor and the doctor said\
                   No more monkeys jumping on the bed!')"    
                 ],
-        resources: [{
-                type: "video",
-                url: "https://www.youtube.com/watch?v=Vr-L6q4vbWU"
+        "resources": [{
+                "type": "video",
+                "url": "https://www.youtube.com/watch?v=Vr-L6q4vbWU"
                 }, {
-                type: "article",
-                url: "http://motherhood.modernmom.com/fun-ways-teach-math-three-year-old-15797.html"
+                "type": "article",
+                "url": "http://motherhood.modernmom.com/fun-ways-teach-math-three-year-old-15797.html"
                 }, {
-                type: 'target',
-                url:"product_id"
+                "type": 'target',
+                "id":"204-03-2169"
                 }
                 
         ],
-        milestones: ["counting"],
-        upvotes: 27, 
-        tags: [
+        "milestones": ["counting"],
+        "upvotes": 27, 
+        "tags": [
                "counting", 
                "math", 
                "music", 
