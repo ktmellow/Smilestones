39a13594a9e3a1a09c69a5f5629edaa44c2df8aa
ktmellow <letsspamkt@gmail.com>
2015-10-24 22:15:00 -0700

    saving models

---
+++ models/profile.js
@@ -0,0 +1,65 @@
+module.exports = {   
+    "001": = {
+        "fullName": “Clarence Leung”,
+        "profilePhoto": "https://avatars2.githubusercontent.com/u/686352?v=3&s=460",
+        "about": 
+        {
+            "region": “San Mateo Convention Center”,
+            "age": “30”,
+            "bio": “As a single father, raising Sophie by myself, I needed all the help I could get. \
+             My jobs don't pay well, so we live paycheck to paycheck. \
+             I want to make sure that my daughter can afford the life I never had. \
+             Every morning I drop a quarter into a piggy bank labelled Sophie's Educational Fund. \
+             I want her to grow up to be a confident leader and passionate learner. So every moment I spend with her, I make it count. \
+             I have always relied on my close friends and family for advice but sometimes even they didn't have all the answers. \
+             After all, it takes a village to raise a child. And that's when I found Smilestones.”
+        }
+        "recipesInvolved": [123] // parent documents each recipe they are trying: their own and others
+        "badges": [
+             "newPublisher",
+             "100DayStreak",
+             "popularRecipe"
+        ],
+        "milestones": [{
+             "name": "Sophia",
+             "text": " learned to count!",
+             "date": "October 22, 2015",
+             "image": "" }, {
+             "name": "Colette",
+             "text": " said her first word!",
+             "date": "October 20, 2015",
+             "image": ""
+             }
+        ],
+        "activity": {[
+             {
+              "type": "upvote",
+              "id": 121,
+              "title": "Potty Training Difficulties with my Child",
+              "description": "I've always had difficulties teaching my son to use the toilet correctly. Whether it's in the middle \
+                            of class or in the middle of the street, my son always has to go at the worst times! Here are some \
+                            of the tricks I've found to turn a potty training disaster into a potty training utopia...",
+              "image": ""
+            },{
+              "type": "publish",
+              "id": 122,
+              "title": "My Daughter is too Brilliant",
+              "description": "My daughter is too brilliant and I don't know what to do with her. She's graduated college at the age of 4 \
+                              and won the Nobel Peace Prize on Thursday. Here's what I used to get her to this point.",
+              "resources": [{
+                            "type": "article",
+                            "title": "The Unique Inner Lives of Gifted Children",
+                            "description": "The article talks about how my gifted child is too unique and too amazing.", 
+                            "image": ""
+                            }, {
+                            "type": "Toy",
+                            "title": "Star Wars: The Force Awakens Titanium First Order TIE Fighter",
+                            "description": "This Star Wars toy boosted my daughter's IQ by 200 points in the course of a single day.",
+                            "image": ""
+                            }
+              ]
+          }
+        ]},        
+    }
+};
+
